821a004a132239e15f843af7cf23de4995d66ef6
Sidhra <sidhra.cader@hotmail.com>
2018-02-13 23:08:17 +1100

    List of characters and some styles using bootstrap

--- app/components/App.js
+++ app/components/App.js
@@ -1,11 +1,41 @@
 import React from 'react'
 import Character from './Character'
 
-export default function App (){
-  return (
+export default class App extends React.Component{
+
+  constructor(){
+    super()
+    this.state = {
+      characters: []
+    }
+  }
+
+  componentDidMount(){
+    fetch("http://localhost/star-wars-api/api.php")
+    .then(res => res.json())
+    .then(response => {
+      this.setState({characters: response.results})
+    })
+  }
+
+  render(){
+    return (
       <div>
-        <Character />
-        <Character />
+        <div className="row">
+          {this.state.characters.map((character, index) =>
+            (
+                <div
+                  key={index}
+                  className="col-4">
+                  <Character
+                    name={character.name}
+                    index={index}
+                    detailsHandler={this.onClickHandler} />
+                </div>
+            ))}
+        </div>
       </div>
     )
+
+  }
 }
--- app/components/Character.js
+++ app/components/Character.js
@@ -1,11 +1,17 @@
 import React from 'react'
 
-export default function Character(){
+export default function Character(props){
   return (
-    <div>
-      <img src="http://via.placeholder.com/250x250" />
-      <p>Luke Skywalker</p>
-      <button>Details</button>
+    <div className="card" style={{width: '250px', margin: '10px'}}>
+      <img className="card-img top" src="http://via.placeholder.com/250x250" />
+      <div className="card-body">
+        <p className="card-title">{props.name}</p>
+        <button
+          className="btn btn-primary"
+          onClick={props.detailsHandler}
+          id={props.index}>
+            Details</button>
+      </div>
     </div>
   )
 }
