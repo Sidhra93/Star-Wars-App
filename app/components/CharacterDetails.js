4900b2c0b596ccdf1e6a37e92f30918aca6d5b8f
Sidhra <sidhra.cader@hotmail.com>
2018-02-13 23:09:18 +1100

    Character Details component to view details

---
+++ app/components/CharacterDetails.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+export default function CharacterDetails(props){
+  return (
+    <div className="row">
+      <div className="col">
+        <h5>{props.name}</h5>
+        <p>Gender: {props.gender}</p>
+        <p>Height: {props.height}</p>
+        <p>Birth Year: {props.birthYear}</p>
+        <p>Hair Color: {props.hairColor}</p>
+
+      </div>
+    </div>
+  )
+}
